c47e41db634a5d06a6695c5ebb3cf4512f27ccf3
Pigandgirl <1746276724@qq.com>
2023-10-17 21:01:29 +0800

    完成底部自定义菜单栏的制作

--- miniprogram-2/custom-tab-bar/index.js
+++ miniprogram-2/custom-tab-bar/index.js
@@ -1,24 +1,47 @@
 // custom-tab-bar/index.js
 Component({
 
-        /**
-         * 组件的属性列表
-         */
-        properties: {
+  /**
+   * 组件的属性列表
+   */
+  properties: {
 
-        },
+  },
 
-        /**
-         * 组件的初始数据
-         */
-        data: {
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    selected: null, //当前选中
+    //
+    list: [{
+        'pagePath': "/pages/index/index",
+        "iconPath": "/images/icon/未选择主页icon.png",
+        "text": '首页'
+      },
+      {
+        'pagePath': "/pages/about/index",
+        "iconPath": "/images/icon/未选择猫icon.png",
+        "text": ' 公司简介 ',
+      },
+      {
+        'pagePath': "/pages/contact/index",
+        "iconPath": "/images/icon/咨询.png",
+        "text": '联系',
+      }
+    ]
+  },
 
-        },
-
-        /**
-         * 组件的方法列表
-         */
-        methods: {
-
-        }
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    switchTab(e) {
+      const { index,url }   = e.currentTarget.dataset;
+      if (this.data.selected == index || index == undefined) return;
+      wx.switchTab({
+        url,
+      })
+    }
+  }
 })
